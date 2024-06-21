ef70c4ee65277a101a2d886177f8e538b7a8aedd
jack <ldy0909061@gmail.com>
2024-06-21 09:02:41 +0400

    feat: add connect to wallet method

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,3 +10,19 @@ export function shortAddress(address?: string, len = 5) {
   if (address.length <= len * 2) return address;
   return address.slice(0, len) + "..." + address.slice(address.length - 5);
 }
+
+export type WalletType = 'metamask'|'trust'|'okx'|'coinbase'|'tomo'
+export const type2InjectorMap = {
+  metamask: 'ethereum',
+  trust: 'trustwallet',
+  okx: 'okxwallet',
+  coinbase: 'coinbaseWalletExtension',
+  tomo: 'tomo_evm'
+}
+export const connectToWallet = async (walletType: WalletType) => {
+  if (!type2InjectorMap[walletType]) {
+    throw new Error('Wallet not install')
+  }
+  const accounts = await ((window as any)[type2InjectorMap[walletType]]).request({ method: 'eth_requestAccounts' })
+  return accounts[0]
+}
