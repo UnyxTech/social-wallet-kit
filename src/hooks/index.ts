6ee9cadb741663123fa20d4646a9f2c2f2705ea2
jack <ldy0909061@gmail.com>
2024-07-14 17:40:29 +0800

    fix: use tomosdk init method

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,16 +1,14 @@
-import { EthereumProvider } from "@tomo-inc/social-wallet-sdk"
 import { useEffect, useState } from "react"
 
-let sdkCache: any
 export const useTomoSDK = () => {
-  const [tomoSDK, setTomoSDK] = useState<any>(sdkCache)
+  const [tomoSDK, setTomoSDK] = useState<any>()
   useEffect(() => {
     const init = async () => {
       if (tomoSDK) {
         return
       }
 
-      const {TomoSDK} = await import('@tomo-inc/social-wallet-sdk');
+      const {TomoSDK, EthereumProvider} = await import('@tomo-inc/social-wallet-sdk');
       TomoSDK.setDevApi()
 
       const chainConfig = {
@@ -18,11 +16,10 @@ export const useTomoSDK = () => {
         rpcUrl: 'https://cloudflare-eth.com',
       }
       const ethProvider = new EthereumProvider(chainConfig)
-      sdkCache = new TomoSDK({
+      setTomoSDK(TomoSDK.init({
         clientId: 'bCMfq7lAMPobDhf6kWAHAPtO5Ct6YuA77W9SzhjUixFwOOi0f92vsdJpkAhn0W4tg8TVSeTNUSvBOC3MXYRuIH0Z',
         ethereumProvider: ethProvider
-      })
-      setTomoSDK(sdkCache)
+      }))
     }
     init()
   }, [])
