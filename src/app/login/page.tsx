5de0b8b93d75ede838f219ce4f3c0a7a4451fb18
Taneeya <2240351540@qq.com>
2024-07-17 13:34:23 +0800

    add evm test button

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,30 +1,48 @@
 "use client";
 import { useState } from "react";
-import { Web3 } from 'web3'
+import { Web3 } from "web3";
 import { TButton } from "@/components/tButton";
 import { SignInDialog } from "@/components/dialog/signIn";
 import { useUserStore } from "@/store/user";
 import { shortAddress } from "@/lib/utils";
-import { getAddresses, sendTx, signMessage, switchChain } from "@/lib/eth-actions"
-import { getBtcInfos, sendBitcoin, signBtcMessage, signPsbt } from "@/lib/btc-actions"
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
+import {
+  getAddresses,
+  sendTx,
+  sendErc20,
+  signMessage,
+  switchChain,
+  signTypeData,
+} from "@/lib/eth-actions";
+import {
+  getBtcInfos,
+  sendBitcoin,
+  signBtcMessage,
+  signPsbt,
+} from "@/lib/btc-actions";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 import { useTomoSDK } from "@/hooks";
 interface IProps {}
 
 const Login: React.FC<IProps> = () => {
   const address = useUserStore((state) => state.address);
-  const tomoSDK = useTomoSDK()
+  const tomoSDK = useTomoSDK();
   const setAddress = useUserStore((state) => state.setAddress);
   const [openSignIn, setOpenSignIn] = useState<boolean>(false);
   const logout = () => {
     setAddress(undefined);
-  }
+  };
 
   return (
     <div className="bg-black h-full flex flex-col items-center">
       <div className="p-[24px] w-full flex justify-end">
-        {
-          !address &&
+        {!address && (
           <TButton
             className="px-[16px] w-fit"
             type="blue"
@@ -32,9 +50,8 @@ const Login: React.FC<IProps> = () => {
           >
             {address ? shortAddress(address) : "Connect Wallet"}
           </TButton>
-        }
-        {
-          address &&
+        )}
+        {address && (
           <NavigationMenu>
             <NavigationMenuList>
               <NavigationMenuItem>
@@ -44,7 +61,9 @@ const Login: React.FC<IProps> = () => {
                 <NavigationMenuContent className="!bg-white">
                   <NavigationMenuLink>
                     <div
-                      onClick={() => {logout()}}
+                      onClick={() => {
+                        logout();
+                      }}
                       className="w-[90px] py-[12px] px-[16px] cursor-pointer"
                     >
                       Log Out
@@ -54,18 +73,17 @@ const Login: React.FC<IProps> = () => {
               </NavigationMenuItem>
             </NavigationMenuList>
           </NavigationMenu>
-        }
+        )}
       </div>
-      {
-        address &&
+      {address && (
         <div>
           <div className="p-[24px] w-full flex justify-start">
             <TButton
               className="px-[16px] w-fit"
               type="blue"
               onClick={() => {
-                const web3 = new Web3(tomoSDK.ethereumProvider)
-                signMessage(web3, 'hello', address)
+                const web3 = new Web3(tomoSDK.ethereumProvider);
+                signMessage(web3, "hello", address);
               }}
             >
               evm personla sign
@@ -74,8 +92,18 @@ const Login: React.FC<IProps> = () => {
               className="px-[16px] w-fit ml-[8px]"
               type="blue"
               onClick={() => {
-                const web3 = new Web3(tomoSDK.ethereumProvider)
-                sendTx(web3, address, address, '0.01')
+                const web3 = new Web3(tomoSDK.ethereumProvider);
+                signTypeData(web3, address);
+              }}
+            >
+              evm sign type data
+            </TButton>
+            <TButton
+              className="px-[16px] w-fit ml-[8px]"
+              type="blue"
+              onClick={() => {
+                const web3 = new Web3(tomoSDK.ethereumProvider);
+                sendTx(web3, address, address, "0.00001");
               }}
             >
               evm send tx with viem
@@ -83,8 +111,18 @@ const Login: React.FC<IProps> = () => {
             <TButton
               className="px-[16px] w-fit ml-[8px]"
               type="blue"
+              onClick={() => {
+                const web3 = new Web3(tomoSDK.ethereumProvider);
+                sendErc20(web3);
+              }}
+            >
+              evm send erc20
+            </TButton>
+            <TButton
+              className="px-[16px] w-fit ml-[8px]"
+              type="blue"
               onClick={async () => {
-                getAddresses(tomoSDK.ethereumProvider)
+                getAddresses(tomoSDK.ethereumProvider);
               }}
             >
               evm get address
@@ -94,7 +132,7 @@ const Login: React.FC<IProps> = () => {
               className="px-[16px] w-fit ml-[8px]"
               type="blue"
               onClick={async () => {
-                switchChain(tomoSDK.ethereumProvider)
+                switchChain(tomoSDK.ethereumProvider);
               }}
             >
               evm switch chain
@@ -105,7 +143,7 @@ const Login: React.FC<IProps> = () => {
               className="px-[16px] w-fit"
               type="blue"
               onClick={() => {
-                signBtcMessage(tomoSDK.bitcoinProvider, 'hello')
+                signBtcMessage(tomoSDK.bitcoinProvider, "hello");
               }}
             >
               sign btc message
@@ -114,7 +152,11 @@ const Login: React.FC<IProps> = () => {
               className="px-[16px] w-fit ml-[8px]"
               type="blue"
               onClick={() => {
-                sendBitcoin(tomoSDK.bitcoinProvider, 'tb1pezwd2ke3kk5qhqtkg2kt3vtfr0nhzdeqdhhelp3qswtkuas68pgqw79745', 0.0001)
+                sendBitcoin(
+                  tomoSDK.bitcoinProvider,
+                  "tb1pezwd2ke3kk5qhqtkg2kt3vtfr0nhzdeqdhhelp3qswtkuas68pgqw79745",
+                  0.0001
+                );
               }}
             >
               send bitcoin
@@ -123,7 +165,7 @@ const Login: React.FC<IProps> = () => {
               className="px-[16px] w-fit ml-[8px]"
               type="blue"
               onClick={async () => {
-                signPsbt(tomoSDK.bitcoinProvider, '70779897')
+                signPsbt(tomoSDK.bitcoinProvider, "70779897");
               }}
             >
               signPsbt
@@ -132,16 +174,15 @@ const Login: React.FC<IProps> = () => {
               className="px-[16px] w-fit ml-[8px]"
               type="blue"
               onClick={async () => {
-                getBtcInfos(tomoSDK.bitcoinProvider)
+                getBtcInfos(tomoSDK.bitcoinProvider);
               }}
             >
               get btc infos
             </TButton>
           </div>
         </div>
-        
-      }
-      
+      )}
+
       <SignInDialog open={openSignIn} onClose={() => setOpenSignIn(false)} />
     </div>
   );
--- src/lib/eth-actions.ts
+++ src/lib/eth-actions.ts
@@ -1,31 +1,130 @@
-import { createWalletClient, custom } from 'viem'
+import { createWalletClient, custom } from "viem";
+import abi from "human-standard-token-abi";
 
-export const signMessage = async (web3: any, message: string, account: string) => {
-  const sig = await web3.eth.personal.sign(message, account, '');
-  console.log('sig:', sig)
-}
+export const signMessage = async (
+  web3: any,
+  message: string,
+  account: string
+) => {
+  const sig = await web3.eth.personal.sign(message, account, "");
+  console.log("sig:", sig);
+};
 
-export const sendTx = async (web3: any, account: string, to: string, value: string) => {
-  const resp = await web3.eth.sendTransaction({ from: '0xB86aA614EDc512f4e3147779f964d420b43E44b4', to: '0xB86aA614EDc512f4e3147779f964d420b43E44b4', value: web3.utils.toWei(value, 'ether') })
-	console.log('resp:', resp)
-}
+export const signTypeData = async (web3: any, account: string) => {
+  const params = {
+    typedData: {
+      types: {
+        EIP712Domain: [
+          { name: "name", type: "string" },
+          { name: "version", type: "string" },
+          { name: "chainId", type: "uint256" },
+          { name: "verifyingContract", type: "address" },
+        ],
+        Person: [
+          { name: "name", type: "string" },
+          { name: "wallets", type: "address[]" },
+        ],
+        Mail: [
+          { name: "from", type: "Person" },
+          { name: "to", type: "Person[]" },
+          { name: "contents", type: "string" },
+        ],
+        Group: [
+          { name: "name", type: "string" },
+          { name: "members", type: "Person[]" },
+        ],
+      },
+      domain: {
+        name: "Ether Mail",
+        version: "1",
+        chainId: Number("0x1"),
+        verifyingContract: "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC",
+      },
+      primaryType: "Mail",
+      message: {
+        from: {
+          name: "Cow",
+          wallets: [
+            "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
+            "0xDeaDbeefdEAdbeefdEadbEEFdeadbeEFdEaDbeeF",
+          ],
+        },
+        to: [
+          {
+            name: "Bob",
+            wallets: [
+              "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
+              "0xB0BdaBea57B0BDABeA57b0bdABEA57b0BDabEa57",
+              "0xB0B0b0b0b0b0B000000000000000000000000000",
+            ],
+          },
+        ],
+        contents: "Hello, Bob!",
+      },
+    },
+  };
+  const sig = await web3.eth.signTypedData(account, params);
+  console.log("sig:", sig);
+};
+
+export const sendTx = async (
+  web3: any,
+  account: string,
+  to: string,
+  value: string
+) => {
+  const resp = await web3.eth.sendTransaction({
+    from: "0xB86aA614EDc512f4e3147779f964d420b43E44b4",
+    to: "0xB86aA614EDc512f4e3147779f964d420b43E44b4",
+    value: web3.utils.toWei(value, "ether"),
+  });
+  console.log("resp:", resp);
+};
+
+const getRawTx = ({ nonce, gasPrice, gasLimit, to, chainId, amount }: any) => {
+  return {
+    nonce: "0x" + nonce.toString(16),
+    gasPrice: "0x" + parseInt(gasPrice.toString()).toString(16),
+    gasLimit: "0x" + parseInt(gasLimit.toString()).toString(16),
+    to,
+    chainId: parseInt(chainId),
+    value: "0x" + amount.toString(16),
+  };
+};
+export const sendErc20 = async (web3: any) => {
+  const from = "0xB86aA614EDc512f4e3147779f964d420b43E44b4";
+  const to = "0xB86aA614EDc512f4e3147779f964d420b43E44b4";
+  const amount = "0.01";
+  const contractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
+
+  try {
+    const contract = new web3.eth.Contract(abi, contractAddress);
+    const value = web3.utils.toWei(amount, "ether");
+    const resp = await contract.methods.transfer(to, value).send({
+      from: from,
+    });
+    console.log("resp:", resp);
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export const switchChain = async (provider: any) => {
   const client = createWalletClient({
-    transport: custom(provider)
-  })
-  let chainId = await client.getChainId()
-  console.log('chainId before:', chainId)
-  client.switchChain({id: 56})
-  chainId = await client.getChainId()
-  console.log('chainId after:', chainId)
-}
+    transport: custom(provider),
+  });
+  let chainId = await client.getChainId();
+  console.log("chainId before:", chainId);
+  client.switchChain({ id: 56 });
+  chainId = await client.getChainId();
+  console.log("chainId after:", chainId);
+};
 
 export const getAddresses = async (provider: any) => {
   const client = createWalletClient({
-    transport: custom(provider)
-  })
-  const [address] = await client.requestAddresses()
+    transport: custom(provider),
+  });
+  const [address] = await client.requestAddresses();
   // const [address] = await client.getAddresses()
-  console.log('address:', address)
-}
+  console.log("address:", address);
+};
