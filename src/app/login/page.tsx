43f05868944b2073cdd1731ce09b8e0188276428
tony <ty_bt@live.cn>
2024-10-11 13:43:41 +0800

    fixed version @tomo-inc/tomo-social-react

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -38,7 +38,7 @@ export default function Demo() {
       evmDefaultChainId={1}
       {...tomoProps}
       style={style}
-      useServerSettings={true}
+      // useServerSettings={true}
       // onlySocial={true}
       // socialTypes={['google', 'email']}
       // indexWallets={['metaMask', 'tomoWallet', 'bitcoin_okx', 'allWallet']}
